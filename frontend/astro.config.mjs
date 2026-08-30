d7c76e64a34a88be96dedae79ce3d59d1b3236d8
psdhtmlcss <pochtoviy-yashik@mail.ru>
2026-08-30 15:46:42 +0400

    Часть 8. Настраиваем Server Side Rendering
    В этом уроке переводим Astro-проект с SSG на SSR (Server Side Rendering) и разбираемся, как теперь будет формироваться HTML страниц.

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,10 +3,15 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
+	output: 'server',
+	adapter: node({
+		mode: 'standalone'
+	}),
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
